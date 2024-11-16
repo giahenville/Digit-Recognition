81f4cd5f5a51566e8efab620d928c8ec5f39fb44
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-11-16 16:28:51 -0500

    Read digit files

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt 
 import tensorflow as tf 
 
-# load dataset
-mnist = tf.keras.datasets.mnist 
-# split into training data and testing data
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-# normalize (every value is between 0-1)
-x_train = tf.keras.utils.normalize(x_train, axis=1)
-x_test = tf.keras.utils.normalize(x_test, axis=1)
+# # load dataset
+# mnist = tf.keras.datasets.mnist 
+# # split into training data and testing data
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# # normalize (every value is between 0-1)
+# x_train = tf.keras.utils.normalize(x_train, axis=1)
+# x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 # # neural network
 # model = tf.keras.models.Sequential() 
@@ -30,7 +30,17 @@ x_test = tf.keras.utils.normalize(x_test, axis=1)
 # load model we just saved from above
 model = tf.keras.models.load_model('handwritten.keras')
 
-# evaluate model
-loss, accuracy = model.evaluate(x_test, y_test)
-print(loss)
-print(accuracy)
+# read digit files
+image_number = 1
+while os.path.isfile(f"digits/digit{image_number}.png"):
+    try:
+        img = cv2.imread(f"digits/digit{image_number}.png")[:,:,0]
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"This digit is most likely {np.argmax(prediction)}")
+        plt.imshow(img[0], cmap=plt.cm.binary)
+        plt.show()
+    except:
+        print("Error")
+    finally:
+        image_number += 1
